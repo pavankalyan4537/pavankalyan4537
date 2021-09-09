aabf2ec71c3677dffa309d8c4b8997a57c3b23f9
Pavan <pavanpls777@gmail.com>
2021-09-09 10:05:34 +0530

    added ten lines of code

--- testing.py
+++ testing.py
@@ -1,2 +1,13 @@
 print("hello pavan welcome to github")
 print("this is pavan")
+
+def fact(num):
+    if num==1:
+        return 1
+    else:
+        return num*fact(num-1)
+
+a=fact(5)
+print("factorial",a)
+
+
